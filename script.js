51f0a011a1fbe224027ff3ad260a32fcf2472eb9
Davit Kurdadze <demetre.kurdadze20b@geolab.edu.ge>
2022-02-06 16:14:16 +0400

    fixed burgerBar functional

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 const mainMenu = document.querySelector('.Wrapper');
 const showMenu = document.querySelector('.burgerDiv');
 const closeMenu = document.querySelector('#closeMenu')
+let index = 1
+showMenu.addEventListener('click',() =>{
+   
+        if(index == 1)
+        {
+            mainMenu.style.display = 'block';
+            index = 0;
+        }else if (index == 0){
+            mainMenu.style.display = 'none';
+            index = 1;
+        }
+});
 
-showMenu.addEventListener('click',show);
-closeMenu.addEventListener('click',close);
 
-function show(){
-    mainMenu.style.display = 'flex';
-    mainMenu.style.top = '0';
-}
-function close(){
-   mainMenu.style.top = '-100%';
-}
